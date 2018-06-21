5b3a32e09e2c9116470a7d01a03b52703e72c66b
noking <noking50@btsdesign.com.tw>
2018-06-21 18:12:56 +0800

    provider

--- src/FileUpload/FileUploadServiceProvider.php
+++ src/FileUpload/FileUploadServiceProvider.php
@@ -11,7 +11,7 @@ class FileUploadServiceProvider extends ServiceProvider {
      *
      * @var bool
      */
-    protected $defer = true;
+    protected $defer = false;
 
     public function boot() {
         $this->loadTranslationsFrom(__DIR__ . '/../lang', 'fileupload');
